4071725ba68583178c792a06f709de1d92dc7f60
Your Name <dmmuchai@gmail.com>
2025-01-27 15:03:04 -0800

    Task 1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    # Generate timestamp for the archive name
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Create versions folder if it doesn't exist
+    local("mkdir -p versions")
+
+    # Create the .tgz archive
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(now))
+    if result.failed:
+        return None
+    else:
+        return result
+
+if __name__ == "__main__":
+    do_pack()
