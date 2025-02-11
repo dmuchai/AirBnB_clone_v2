3ab7ba6dc880eea91c0e34b79413123bb2a35c15
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-02-11 01:34:30 -0800

    Task 1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ def hello_hbnb():
     """A function that returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """A function that returns 'HBNB'"""
