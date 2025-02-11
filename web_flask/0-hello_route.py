addbfd82f4ba0547120acd53f9b7d05a96f10b7d
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-02-11 01:18:02 -0800

    Task 0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-# This script initializes a Flask web application with a route that returns "Hello HBNB!" when accessed.
+"""This script initializes a Flask web app that returns "Hello HBNB!"""
 
 from flask import Flask
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Define route with strict_slashes=False
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    This function handles the route "/". It returns the string 'Hello HBNB!' when accessed.
-    """
+    """A function that returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    """
-    This block starts the Flask web application. It listens on all available IP addresses (0.0.0.0)
-    and uses port 5000. The application runs in debug mode to provide detailed error logs.
-    """
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
