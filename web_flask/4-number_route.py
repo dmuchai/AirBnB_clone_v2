27a3f7de589b0ac4a05c746539b2e50181a813d3
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-02-11 02:47:25 -0800

    Task 4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This script initializes a Flask web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """A function that returns 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that returns 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Returns 'C' followed by value of text variable.
+    Underscores in the text are replaced by spaces
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """Returns 'python' followed by value of text variable.
+    Underscores in the text are replaced by spaces
+    The default value of text is 'is cool'
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Returns 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
